f80bf0edade0ba23ef6bb976ef9e7b6ffd787f9e
Rohan Jhunjhunwala <rjhunjhunwala80@berkeley.edu>
2020-01-18 20:16:55 -0800

    added improvements

--- major_parser.py
+++ major_parser.py
@@ -1,5 +1,6 @@
 from html.parser import HTMLParser
 FILENAME = "major_page"
+import csv
 import os
 
 
@@ -11,6 +12,8 @@ class ClassParser(HTMLParser):
         HTMLParser.__init__(self, *args, **kwargs)
         self.isInCourseBlock = False
         self.nesting = 0
+        self.prepped_for_desc = False
+        self.desc = False
 
         self.classes = []
         pass
@@ -24,32 +27,67 @@ class ClassParser(HTMLParser):
             self.classes.append([])
         elif tag == "div":
             self.nesting += 1
+        elif tag == "span" and attrs and "descshow" in attrs[0][1]:
+            self.prepped_for_desc = True
+            # input(":")
 
+        self.tag = tag
+        self.attrs = attrs
 
         pass
-
-
+    def clean(self, st):
+        ALL = "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        return "".join(ch if ch in ALL else " " for ch in st)
     def handle_endtag(self, tag):
+
         if tag == "div":
             if self.nesting == 0:
                 self.isInCourseBlock = False
             else:
                 self.nesting -= 1
+        if self.prepped_for_desc and tag == "br":
+            # print("Here!")
+            self.desc = True
+            self.prepped_for_desc = False
         pass
     def handle_data(self, data):
         if self.isInCourseBlock:
-            print(data)
+            # print(data)
+            if self.tag == "span" and self.attrs and self.attrs[0][1] == "code":
+                self.classes[-1].append(self.clean(data))
+            if self.desc:
+                self.desc = False
+                self.classes[-1].append(data[1:])
+
+
         pass
 
 def process(major_name):
-    os.system("curl http://guide.berkeley.edu/undergraduate/degree-programs/{}/#majorrequirementstext >> data/{}/{}".format(major_name, major_name, major_name))
+
+    os.system("curl http://guide.berkeley.edu/undergraduate/degree-programs/{}/#majorrequirementstext >> data/{}".format(major_name, major_name))
+
     parser = ClassParser()
+
 
     text = ""
     with open("data/" + major_name, "r") as f:
         text = f.read()
 
     parser.feed(text)
+    print(parser.classes)
+    with open("data/" + major_name + "_vertices.csv", "w") as vertices:
+        fieldnames = ["name", "size", "about"]
+        writer = csv.DictWriter(vertices, fieldnames=fieldnames)
+        writer.writeheader()
+        for bk_class in parser.classes:
+            name = bk_class[0]
+            about = "" if len(bk_class) == 2 else bk_class[2]
+            writer.writerow({"name":name, "size":"1", "about":about})
+    with open("data/" + major_name + "_edges.csv", "w") as vertices:
+        fieldnames = ["source", "target", "type", "weight"]
+        writer = csv.DictWriter(vertices, fieldnames=fieldnames)
+        writer.writeheader()
+    # print(parser.classes)
 
 def print_cleaned(st):
     def cap_first_letter(st):
@@ -91,11 +129,14 @@ from datetime import datetime
 from pytz import timezone
 import pytz
 
+
 parser = MajorParser()
 
 text = ""
 with open(FILENAME, "r") as f:
     text = f.read()
+
+
 
 parser.feed(text)
 
